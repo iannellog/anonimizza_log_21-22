7961baf93b969c645657ae74e853a7bc4cdf06cf
Paolo Ruggirello <ruggirello99@hotmail.it>
2021-10-29 10:05:18 +0200

    Now grouping indexes by user and saving the index as a string of 5 characters

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def getJsonData(fileName):
     return data
 
 
+"""
 def anonymizeAndGetAssociations(jsonData):
     codeToUserName = {}
     userIndex = 1
@@ -32,6 +33,35 @@ def anonymizeAndGetAssociations(jsonData):
             userLog.remove(userLog[2])  # Removing involved user info
             userIndex += 1
     return codeToUserName
+"""
+
+
+def reformatUserNameAndCompleteUserInfo(userLog):
+    if userLog[1] == '-' and userLog[2] != '-':
+        temp = userLog[2]
+        userLog[2] = userLog[1]
+        userLog[1] = temp
+
+
+def anonymizeAndGetAssociations(jsonData):
+    userNameToCode = {}
+    userIndex = 1
+    for logDays in jsonData:
+        for userLog in logDays:
+            currentUserIndex = -1  # -1 means error
+            reformatUserNameAndCompleteUserInfo(userLog)
+            if userLog[1] in userNameToCode:
+                currentUserIndex = userNameToCode[userLog[1]]
+            if not userLog[1] in userNameToCode:  # Checks if user is present in dictionary
+                currentUserIndex = str(userIndex).zfill(5)
+                userNameToCode[userLog[1]] = currentUserIndex  # Create a string of 5 characters adding missing zero before the number
+                userIndex += 1
+            if currentUserIndex == -1:
+                print('Error, cannot convert userName to unique index')
+            else:
+                userLog[1] = currentUserIndex
+            userLog.remove(userLog[2])  # Removing involved user info
+    return userNameToCode
 
 
 def saveOnFile(fileName, dumpData, indent=3):
