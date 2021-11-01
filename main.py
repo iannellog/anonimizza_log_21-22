ebe1d417748754d1b2a34851085118960011bfcd
Simone Pio Caronia <spcaronia@gmail.com>
2021-11-01 19:27:58 +0100

    completed personal solution for anonymization of logs

--- main.py
+++ main.py
@@ -1,35 +1,81 @@
 """
-Created on Oct. 29, 2021
-by Giulio Iannello
+Created on Nov. 1, 2021
+by Simone Pio Caronia
 
-Programma che anonimizza un file di log prdotto da moodle
+Programma che anonimizza un file di log prodotto da moodle
+
+Info on Json:
+    - Data/Ora
+    - Nome completo dell'utente
+    - Utente coinvolto
+    - Contesto dell'evento
+    - Componente
+    - Evento
+    - Descrizione
+    - Origine
+    - Indirizzo IP
 """
 
 import json
+import uuid
 
-fin = open('/Users/iannello/Home/Didattica/Programmazione/code/[21-22]/anonimizza_log_21-22/indata/anonimizza_test1.json')
-log_list = json.load(fin)
-fin.close()
+INDEX_COMPLETE_NAME = 1
+INDEX_AFFECTED_USER = 2
 
-log_list = log_list[0]
 
-tab_codici = {}
-codice = 1
-for log in log_list:
-    if not log[1] in tab_codici:
-        tab_codici[log[1]] = str(codice).zfill(5)
-        codice += 1
+def read_and_return_json(path):
+    f_in = open(path)
+    data = json.load(f_in)
+    f_in.close()
+    return data
 
-for i, log in enumerate(log_list):
-    log[1] = tab_codici[log[1]]
-    log_list[i].pop(2)
 
-fout = open('/Users/iannello/Home/Didattica/Programmazione/code/[21-22]/anonimizza_log_21-22/indata/anonimizza_test1_tab_codici.json', 'w')
-json.dump(tab_codici, fout, indent=3)
-fout.close()
+def get_logs_and_users_dict(json_logs_from_file):
+    dict_user = dict()
+    anonymous_all_logs = list()
 
-fout = open('/Users/iannello/Home/Didattica/Programmazione/code/[21-22]/anonimizza_log_21-22/indata/anonimizza_test1_anonimizzato.json', 'w')
-json.dump(log_list, fout, indent=3)
-fout.close()
+    for daily_logs_list in json_logs_from_file:
+        anonymous_daily_logs = list()
 
-print('Fine')
+        for log in daily_logs_list:
+            user_id = get_user_anonymous_uuid_and_insert_if_not_present(log, dict_user)
+            log[INDEX_COMPLETE_NAME] = user_id  # anonymous uuid in place of "Nome completo dell'utente"
+            del log[INDEX_AFFECTED_USER]  # removing "Utente coinvolto"
+            anonymous_daily_logs.append(log)
+
+        anonymous_all_logs.append(anonymous_daily_logs)
+
+    return anonymous_all_logs, dict_user
+
+
+def get_user_anonymous_uuid_and_insert_if_not_present(log, dict_user):
+    user = log[INDEX_COMPLETE_NAME] if log[INDEX_COMPLETE_NAME] != '-' else log[INDEX_AFFECTED_USER]
+    if user in dict_user.keys():
+        return dict_user[user]
+    else:
+        new_id = uuid.uuid5(uuid.NAMESPACE_URL, user)  #creation of UUID value based on user value
+        dict_user[user] = str(new_id)
+        return dict_user[user]
+
+
+def save_to_path(values, path, indent=3):
+    success = True
+    try:
+        f_out = open(path, 'w')
+        json.dump(values, f_out, indent=indent)
+        f_out.close()
+    except NameError:
+        success = False
+
+    return success
+
+
+if __name__ == '__main__':
+    print('Starting to anonymize logs...')
+    jsonData = read_and_return_json('indata/anonimizza_test1.json')
+    logs, users_dict = get_logs_and_users_dict(jsonData)
+    if save_to_path(logs, 'indata/anonimizza_test1_anonimizzato.json') \
+            and save_to_path(users_dict, 'indata/anonimizza_test1_tab_codici.json'):
+        print('Operations completed successfully!')
+    else:
+        print('Ops! Something went wrong!')
