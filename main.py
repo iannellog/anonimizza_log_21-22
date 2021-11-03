6d68e69e1f06b275971749b260dd36a867647c39
Maria Elena Contini <mariaelenacontini@MacBook-Pro-di-Maria-2.local>
2021-11-03 14:02:02 +0100

    primo commit soluzione

--- main.py
+++ main.py
@@ -7,29 +7,33 @@ Programma che anonimizza un file di log prdotto da moodle
 
 import json
 
-fin = open('/Users/iannello/Home/Didattica/Programmazione/code/[21-22]/anonimizza_log_21-22/indata/anonimizza_test1.json')
+fin = open('/Users/mariaelenacontini/Desktop/programmazione/anonimizza_git/anonimizza_log_21-22/indata/anonimizza_test1.json')
 log_list = json.load(fin)
-fin.close()
-
 log_list = log_list[0]
 
+# creo un dizionario in cui metto tutti i nomi (secondo elemento -> 1)
 tab_codici = {}
 codice = 1
+
 for log in log_list:
-    if not log[1] in tab_codici:
-        tab_codici[log[1]] = str(codice).zfill(5)
-        codice += 1
-
-for i, log in enumerate(log_list):
-    log[1] = tab_codici[log[1]]
+    if not log[1] in tab_codici: # considero il secondo (->1) elemento di ogni elemento
+        tab_codici[log[1]] = str(codice).zfill(5) #per riempire di zeri fino a 5 cifre
+        codice += 1 
+ 
+  # enumerate mi permette di scorrere anche un indice      
+for i,log in enumerate(log_list):
+    log[1] = tab_codici[log[1]] # lo sostituisco
+     # per saltare il 3° elemento
+    #log_list[i] = log_list[i][:2] + log_list[3:]
     log_list[i].pop(2)
-
-fout = open('/Users/iannello/Home/Didattica/Programmazione/code/[21-22]/anonimizza_log_21-22/indata/anonimizza_test1_tab_codici.json', 'w')
-json.dump(tab_codici, fout, indent=3)
+ # salviamo tabella dei codici e file anonimizzato   
+fout = open('/Users/mariaelenacontini/Desktop/programmazione/anonimizza_git/anonimizza_log_21-22/indata/tab_codici.json', 'w')
+json.dump(tab_codici,fout, indent=3 )
 fout.close()
 
-fout = open('/Users/iannello/Home/Didattica/Programmazione/code/[21-22]/anonimizza_log_21-22/indata/anonimizza_test1_anonimizzato.json', 'w')
-json.dump(log_list, fout, indent=3)
+fout = open('/Users/mariaelenacontini/Desktop/programmazione/anonimizza_git/anonimizza_log_21-22/indata/lista_anonimizzata.json', 'w')
+json.dump(log_list,fout, indent=3 )
 fout.close()
+
 
 print('Fine')
