d2e951bc9d12c92d680c51ee5dc6607ca6ae9335
Maria Elena Contini <mariaelenacontini@MacBook-Pro-di-Maria-2.local>
2021-11-05 10:38:58 +0100

    genelizzazione dei path

--- main.py
+++ main.py
@@ -23,14 +23,14 @@ Se non è possibile individuare l'utente con 'Nome completo dell'utente'
 
 import json
 
-fin = open('/Users/mariaelenacontini/Desktop/programmazione/anonimizza_git/anonimizza_log_21-22/indata/anonimizza_test1.json')
+fin = open('indata/anonimizza_test1.json')
 log_list = json.load(fin)
 log_list = log_list[0]
 
 # creo un dizionario in cui metto tutti i nomi (secondo elemento -> 1)
+
 tab_codici = {}
 codice = 1
-
 
 for log in log_list:
     #inverto i 2° ed il 3° se il 2° è uguale a '-'
@@ -40,20 +40,21 @@ for log in log_list:
         tab_codici[log[1]] = str(codice).zfill(5) #per riempire di zeri fino a 5 cifre
         codice += 1 
  
-  # enumerate mi permette di scorrere anche un indice      
-for i,log in enumerate(log_list):
+   
+for i,log in enumerate(log_list): # enumerate mi permette di scorrere anche un indice     
     log[1] = tab_codici[log[1]] # lo sostituisco
-     # per saltare il 3° elemento
+    # per saltare il 3° elemento
     #log_list[i] = log_list[i][:2] + log_list[3:]
     log_list[i].pop(2)
- # salviamo tabella dei codici e file anonimizzato   
-fout = open('/Users/mariaelenacontini/Desktop/programmazione/anonimizza_git/anonimizza_log_21-22/indata/tab_codici.json', 'w')
+
+# salviamo tabella dei codici e file anonimizzato   
+
+fout = open('indata/tab_codici.json', 'w')
 json.dump(tab_codici,fout, indent=3 )
 fout.close()
 
-fout = open('/Users/mariaelenacontini/Desktop/programmazione/anonimizza_git/anonimizza_log_21-22/indata/lista_anonimizzata.json', 'w')
+fout = open('indata/lista_anonimizzata.json', 'w')
 json.dump(log_list,fout, indent=3 )
 fout.close()
-
 
 print('Fine')
