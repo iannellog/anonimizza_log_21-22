034b8d64e4544f7e8dd995149faf65cb8e2a9adf
Maria Elena Contini <mariaelenacontini@MacBook-Pro-di-Maria-2.local>
2021-11-07 10:38:27 +0100

    creazione di sottoprogrammi di lettura e di salvataggio dei file json e di user missing

--- main.py
+++ main.py
@@ -22,11 +22,41 @@ Se non è possibile individuare l'utente con 'Nome completo dell'utente'
 """
 
 import json
+import sys
 
-fin = open('indata/anonimizza_test1.json')
-log_list = json.load(fin)
+# questo sottoprogramma legge il file json e salva i dati in una lista di liste
+
+def read_json_file(file):
+    try:
+        fin = open(file)
+        log_list = json.load(fin)
+        fin.close()
+        return log_list
+    except:
+        print('*** errore *** Non è stato caricato alcun file!')
+        sys.exit()
+        
+# questo sottoprogramma salva i dati in un nuovo file json
+
+def save_json_file(file, data):
+    try:
+        fout = open(file, 'w')
+        json.dump(data,fout, indent=3)
+        fout.close()
+    except:
+        print('*** errore *** Qualcosa è andato storto durante la creazione del nuovo file')
+        sys.exit()
+        
+# questo sottoprogramma scambia l'user name con il dato involved user se il primo è uguale a '_'
+
+def user_missing(log):
+    if(log[INDEX_UserName]=='-' and log[INDEX_InvolvedUser]!='-'):
+        log[INDEX_UserName],log[INDEX_InvolvedUser]=log[INDEX_InvolvedUser],log[INDEX_UserName]
+
+    
+JsonFile = 'indata/anonimizza_test1.json'
+log_list = read_json_file(JsonFile)
 log_list = log_list[0]
-
 INDEX_UserName = 1 # quello che voglio anonimizzare
 INDEX_InvolvedUser = 2 # quello che voglio eliminare
 
@@ -35,13 +65,10 @@ tab_codici = {}
 codice = 1
 
 for log in log_list:
-    #inverto i 2° ed il 3° se il 2° è uguale a '-'
-    if(log[INDEX_UserName]=='-' and log[INDEX_InvolvedUser]!='-'):
-        log[INDEX_UserName],log[INDEX_InvolvedUser]=log[INDEX_InvolvedUser],log[INDEX_UserName]
-    if not log[1] in tab_codici: # considero il secondo (->1) elemento di ogni elemento
+    user_missing(log)
+    if not log[INDEX_UserName] in tab_codici: # considero il secondo (->1) elemento di ogni elemento
         tab_codici[log[INDEX_UserName]] = str(codice).zfill(5) #per riempire di zeri fino a 5 cifre
         codice += 1 
- 
    
 for i,log in enumerate(log_list): # enumerate mi permette di scorrere anche un indice     
     log[INDEX_UserName] = tab_codici[log[INDEX_UserName]] # lo sostituisco
@@ -50,13 +77,7 @@ for i,log in enumerate(log_list): # enumerate mi permette di scorrere anche un i
     log_list[i].pop(INDEX_InvolvedUser)
 
 # salviamo tabella dei codici e file anonimizzato   
-
-fout = open('indata/tab_codici.json', 'w')
-json.dump(tab_codici,fout, indent=3 )
-fout.close()
-
-fout = open('indata/lista_anonimizzata.json', 'w')
-json.dump(log_list,fout, indent=3 )
-fout.close()
+save_json_file('indata/tab_codici.json', tab_codici)
+save_json_file('indata/lista_anonimizzata.json', log_list)
 
 print('Fine')
