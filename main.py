4333a3f00e7938425665df1f5b44ec38b11c7aec
Maria Elena Contini <mariaelenacontini@MacBook-Pro-di-Maria-2.local>
2021-11-07 10:07:00 +0100

    generalizzazione indici di user name e involved user

--- main.py
+++ main.py
@@ -27,25 +27,27 @@ fin = open('indata/anonimizza_test1.json')
 log_list = json.load(fin)
 log_list = log_list[0]
 
-# creo un dizionario in cui metto tutti i nomi (secondo elemento -> 1)
+INDEX_UserName = 1 # quello che voglio anonimizzare
+INDEX_InvolvedUser = 2 # quello che voglio eliminare
 
+# creo un dizionario in cui metto tutti i nomi (secondo elemento -> 1)
 tab_codici = {}
 codice = 1
 
 for log in log_list:
     #inverto i 2° ed il 3° se il 2° è uguale a '-'
-    if(log[1]=='-' and log[2]!='-'):
-        log[1],log[2]=log[2],log[1]
+    if(log[INDEX_UserName]=='-' and log[INDEX_InvolvedUser]!='-'):
+        log[INDEX_UserName],log[INDEX_InvolvedUser]=log[INDEX_InvolvedUser],log[INDEX_UserName]
     if not log[1] in tab_codici: # considero il secondo (->1) elemento di ogni elemento
-        tab_codici[log[1]] = str(codice).zfill(5) #per riempire di zeri fino a 5 cifre
+        tab_codici[log[INDEX_UserName]] = str(codice).zfill(5) #per riempire di zeri fino a 5 cifre
         codice += 1 
  
    
 for i,log in enumerate(log_list): # enumerate mi permette di scorrere anche un indice     
-    log[1] = tab_codici[log[1]] # lo sostituisco
+    log[INDEX_UserName] = tab_codici[log[INDEX_UserName]] # lo sostituisco
     # per saltare il 3° elemento
     #log_list[i] = log_list[i][:2] + log_list[3:]
-    log_list[i].pop(2)
+    log_list[i].pop(INDEX_InvolvedUser)
 
 # salviamo tabella dei codici e file anonimizzato   
 
